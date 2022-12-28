0b98aaa8621582c0bb058f42da14fdbfcb2d786b
Daria Belova <ruabld@mail.ru>
2022-12-28 19:19:22 +0300

    рекурсия возврат чисел

---
+++ task64/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int RecNum(int n)
+{
+if (n==1) 
+return 1;
+else
+{ 
+    Console.Write(n + " ");
+    return RecNum(n-1);
+}
+}
+Console.WriteLine(RecNum(N));
