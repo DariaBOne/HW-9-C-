d41ae0846e15976d6dc18005b5e4d28c59e66762
Daria Belova <ruabld@mail.ru>
2022-12-28 20:03:08 +0300

    рекурсия сумма чисел

---
+++ task66/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите меньшее число");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите большее число");
+int N = Convert.ToInt32(Console.ReadLine());
+int RecSum(int a, int b)
+{
+if (a==b) 
+    return b;
+else
+    return a + RecSum(a-1, b);
+}
+if (N>M)
+Console.Write(RecSum(N,M));
+else
+Console.WriteLine("вы неправильно ввели числа");
